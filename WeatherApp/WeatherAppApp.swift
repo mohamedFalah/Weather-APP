a01acafce0b5609af874a97da0ec914076e04ef6
MohamedFalah <mohamshualh@gmail.com>
2020-11-07 07:19:02 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Mohammed Alshulah on 07/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
